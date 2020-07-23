1a42be088932289de2f6968971f6749995a053a5
Marcin <marcin9213@gmail.com>
2020-07-23 21:23:30 +0200

    add dark style & local storage

---
+++ js/app.js
@@ -0,0 +1,12 @@
+var setTheme = localStorage.getItem('theme')
+if (setTheme == null){
+    swapStyle('style-light.css')
+}else{
+    swapStyle(setTheme)
+}
+
+function swapStyle(sheet) {
+    document.getElementById('mystylesheet').href = `/css/${sheet}`;
+    localStorage.setItem('theme', sheet)
+}
+
