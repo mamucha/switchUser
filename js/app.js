48af580b2dda2204b7d6de4cfc3cbdfb145372dc
Marcin <marcin9213@gmail.com>
2020-07-26 11:04:23 +0200

    fix of path file

--- js/app.js
+++ js/app.js
@@ -1,12 +1,11 @@
-var setTheme = localStorage.getItem('theme')
-if (setTheme == null){
-    swapStyle('style-light.css')
-}else{
-    swapStyle(setTheme)
+var setTheme = localStorage.getItem("theme");
+if (setTheme == null) {
+  swapStyle("style-light.css");
+} else {
+  swapStyle(setTheme);
 }
 
 function swapStyle(sheet) {
-    document.getElementById('mystylesheet').href = `/css/${sheet}`;
-    localStorage.setItem('theme', sheet)
+  document.getElementById("mystylesheet").href = `./css/${sheet}`;
+  localStorage.setItem("theme", sheet);
 }
-
